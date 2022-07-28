1038049f16135d9ec68b4193a4bec43f393cc0ee
laiqianqian <15734551856@163.com>
2022-07-28 17:29:35 +0800

    feat: init

--- vue.config.js
+++ vue.config.js
@@ -4,21 +4,21 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
   // css: {
-  //   loaderOptions: {
-  //     stylus: {
-  //       'resolve url': true,
-  //       // 自定义主题场景
-  //       import: [path.resolve(__dirname, './src/theme')]
-  //     },
-  //     postcss: {
-  //       autoprefixer: {},
-  //       plugins: [
-  //         require('postcss-px2rem')({
-  //           remUnit: '37.5',
-  //           propList: ['*']
-  //         })
-  //       ]
-  //     }
+  // loaderOptions: {
+  //   stylus: {
+  //     'resolve url': true,
+  //     // 自定义主题场景
+  //     import: [path.resolve(__dirname, './src/theme')]
+  //   //   },
+  //   postcss: {
+  //     autoprefixer: {},
+  //     plugins: ()=>[
+  //       require('postcss-px2rem')({
+  //         remUnit: '37.5',
+  //         propList: ['*']
+  //       })
+  //     ]
   //   }
+  // }
   // },
 })
